b15a5c270cb0b195d61dae77a23a6e75fdce9f87
software850 <software@gomez.cr>
2025-12-22 21:30:39 +0000

    cambios credito

--- backend/database/seeders/DatabaseSeeder.php
+++ backend/database/seeders/DatabaseSeeder.php
@@ -19,6 +19,8 @@ class DatabaseSeeder extends Seeder
             CrmSeeder::class,
             EnterpriseSeeder::class,
             EnterpriseRequirementSeeder::class,
+            DeductoraSeeder::class,
+            CreditSeeder::class,
         ]);
     }
 }
