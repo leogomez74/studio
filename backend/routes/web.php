70eb49b7ba3641a591561e365798997724395eb2
richard <richard@gomez.cr>
2026-01-05 20:22:40 +0000

    agregar tse a form

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -5,3 +5,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Ruta oculta para formulario de registro de leads
+// Acceso: /Xk9mP2nL
+Route::get('/Xk9mP2nL', function () {
+    return response(file_get_contents(public_path('lead-form.html')))
+        ->header('Content-Type', 'text/html');
+});
